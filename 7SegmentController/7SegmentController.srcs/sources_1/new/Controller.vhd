d22bc2c4cf2467d56ae26f26dd1e330796ab1972
Mathias Maes <mathiasmaesdev@gmail.com>
2019-03-14 07:45:11 +0100

    ADD: Documentation

--- 7SegmentController/7SegmentController.srcs/sources_1/new/Controller.vhd
+++ 7SegmentController/7SegmentController.srcs/sources_1/new/Controller.vhd
@@ -22,6 +22,7 @@ end Controller;
 
 architecture Behavioral of Controller is
 
+    -- Define the component created in 'BCDTo7SegmentConverter.vhd'
     component BCDTo7SegmentConverter port(
         BCD : in Unsigned(3 downto 0);
         SEG : out std_logic_vector(6 downto 0)
@@ -33,18 +34,22 @@ architecture Behavioral of Controller is
     
 begin
     
+    -- Create a converter
+    -- and map it's input and output to the corrosponding signals
     BCDConverter : BCDTo7SegmentConverter port map(
         BCD => BCDin(3 downto 0),
         SEG => SEGout
     );
     
+    -- Check if the switches have been updated
     pInputChanged : process (SW)
     begin  
         -- Check if the 7 segment displays should be on or off 
-        AN(0) <= not(not(SW(7)) and not(SW(8) ) );
-        AN(1) <= not(SW(7) and not(SW(8)));
-        AN(6) <= not(SW(8) and not(SW(7)));
-        AN(7) <= not(SW(7) and SW(8));
+        -- These need to be active low
+        AN(0) <= not(not(SW(7)) and not(SW(8) ) );  -- 0 & 0
+        AN(1) <= not(SW(7) and not(SW(8)));         -- 0 & 1
+        AN(6) <= not(SW(8) and not(SW(7)));         -- 1 & 0
+        AN(7) <= not(SW(7) and SW(8));              -- 1 & 1
         
         -- Set the unused displays off
         AN(2) <= '1';
@@ -52,26 +57,38 @@ begin
         AN(4) <= '1';
         AN(5) <= '1';
         
+        -- Check if the first display is on and the binary input is below 100
        if (unsigned(SW(8 downto 7 )) = 0) and unsigned(SW(6 downto 0)) < 100
        then
+            -- Get the remainder of the binary value from the divison by 10
             BCDin <= unsigned(SW(6 downto 0)) rem 10;
-            
+       --Check if the seccond display is on or the binary input is above 99
        elsif (unsigned(SW(8 downto 7 )) = 1) or unsigned(SW(6 downto 0)) > 99
        then
+            -- Divide by ten
+            -- If this number becomes more than 9 the Converter will return 'E'
             BCDin <= unsigned(SW(6 downto 0)) / 10;
             
+       -- Check if the third display is on and the binary input is below 100
        elsif (unsigned(SW(8 downto 7 )) = 2) and unsigned(SW(15 downto 9)) < 100
        then
+            -- Get the remainder of the binary value from the divison by 10
             BCDin <= unsigned(SW(15 downto 9)) rem 10;
             
+       -- Check if the fourth display is on or the binary input is above 99     
        elsif (unsigned(SW(8 downto 7 )) = 3) or unsigned(SW(15 downto 9)) > 99
        then
+            -- Divide by ten
+            -- If this number becomes more than 9 the Converter will return 'E'
             BCDin <= unsigned(SW(15 downto 9)) / 10;
        end if;
     end process;
     
+    -- Check if the 7 Segment code has been changed
     pSegmentUpdated : process(SEGout)
     begin
+        -- Reverse the displays
+        -- Add a not port because the 7 segment displays are active low
         CA <= not SEGout(6);
         CB <= not SEGout(5);
         CC <= not SEGout(4);
---
+++ 7SegmentController/README.md
@@ -0,0 +1,8 @@
+# 7 Segment Controller
+## Description
+This project is built for the Nexys A7 100t
+
+Control 4 7 Segment displays with dip switches.
+* 7 switches are for the first 2 displays
+* 7 others are for the last 2 displays
+* 2 Switches are used to control which display should light up.
